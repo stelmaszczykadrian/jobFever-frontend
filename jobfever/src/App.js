1b3356d315ae4663cd5ec40cd25df698ff2e24ed
MonikaMarkulis <markulismonika@gmail.com>
2023-03-22 19:20:31 +0100

    Create navbar component

--- jobfever/src/App.js
+++ jobfever/src/App.js
@@ -1,9 +1,12 @@
-import LoginSheet from './components/login/LoginSheet';
+import  ResponsiveAppBar from "./components/navbar";
+import FilterBar from "./components/filter-bar";
 import React from "react";
+import LoginSheet from './components/login/LoginSheet';
+
 function App() {
   return (
     <div>
-        <LoginSheet />
+       <LoginSheet />
     </div>
   );
 }
---
+++ jobfever/src/components/filter-bar.jsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+
+import SearchIcon from '@mui/icons-material/Search';
+import Search from "@mui/icons-material/Search";
+import {InputBase, styled} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
+
+function ResponsiveAppBar() {
+
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }));
+
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                width: '12ch',
+                '&:focus': {
+                    width: '20ch',
+                },
+            },
+        },
+    }));
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Toolbar>
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component="div"
+                    sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                >
+                </Typography>
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                        placeholder="Search…"
+                        inputProps={{ 'aria-label': 'search' }}
+                    />
+                </Search>
+            </Toolbar>
+        </Box>
+    );
+}
+
+export default ResponsiveAppBar;
