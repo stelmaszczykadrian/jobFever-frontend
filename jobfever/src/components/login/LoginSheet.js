12121006f1e3f6216d4e11c21ee214d99633143f
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-03-23 18:07:10 +0100

    Refactor: Deleted unused code.

--- jobfever/src/components/login/LoginSheet.js
+++ jobfever/src/components/login/LoginSheet.js
@@ -15,11 +15,11 @@ import IconButton from '@mui/material/IconButton';
 function LoginSheet() {
     return (
         <div>
-            <div id="container" >
+            <div id="container">
                 <div id="flexedBox"></div>
-                <Sheet style={{ background: 'rgba(29, 25, 23, 0.7)'}}
+                <Sheet style={{background: 'rgba(29, 25, 23, 0.7)'}}
                        sx={{
-                           width: 1/2,
+                           width: 1 / 2,
                            mx: 'auto', // margin left & right
                            my: 0, // margin top & botom
                            py: 3, // padding top & bottom
@@ -33,21 +33,20 @@ function LoginSheet() {
                         <div id="gridedBox">
                             <img id="logo" src={logo}/>
                         </div>
-                        <Typography level="h4" component="h1" sx={{ color: 'rgba(171, 36, 36)', fontWeight: 'bold', textAlign: 'center' }}>
+                        <Typography level="h4" component="h1"
+                                    sx={{color: 'rgba(171, 36, 36)', fontWeight: 'bold', textAlign: 'center'}}>
                             <span>Welcome!</span>
                         </Typography>
-                        <Typography level="body2" component="h1" sx={{ color: 'white', fontWeight: 'bold', textAlign: 'center' }}>
+                        <Typography level="body2" component="h1"
+                                    sx={{color: 'white', fontWeight: 'bold', textAlign: 'center'}}>
                             <span>Sign in to continue.</span>
                         </Typography>
                     </div>
                     <p></p>
                     <FormControl width="40">
-                        <FormLabel sx={{alignSelf: 'center',color: 'white'}}>Email</FormLabel>
+                        <FormLabel sx={{alignSelf: 'center', color: 'white'}}>Email</FormLabel>
                         <Input
-                            // html input attribute
-                            error
-                            id="outlined-error-helper-text"
-                            sx={{border: '1px', width: 2/5, alignSelf: 'center', color: 'grey'}}
+                            sx={{border: '1px', width: 2 / 5, alignSelf: 'center', color: 'grey'}}
                             name="email"
                             type="email"
                             placeholder="jobFever@email.com"
@@ -56,10 +55,7 @@ function LoginSheet() {
                     <FormControl id="inputLighter">
                         <FormLabel sx={{border: '1px', alignSelf: 'center', color: 'white'}}>Password</FormLabel>
                         <Input
-                            error
-                            id="outlined-error-helper-text"
-                            sx={{width: 2/5, alignSelf: 'center', color: 'grey'}}
-                            id="outlined-error"
+                            sx={{width: 2 / 5, alignSelf: 'center', color: 'grey'}}
                             name="password"
                             type="password"
                             placeholder="password"
@@ -67,22 +63,25 @@ function LoginSheet() {
                         <Typography
                             endDecorator={<a style={{color: 'white'}} href="">Forgot a password?</a>}
                             fontSize="sm"
-                            sx={{ color: 'white', fontWeight: 'bold', textAlign: 'center', alignSelf: 'center'}}
+                            sx={{color: 'white', fontWeight: 'bold', textAlign: 'center', alignSelf: 'center'}}
                         ></Typography>
                     </FormControl>
-                    <Button sx={{ ':hover': {
-                    bgcolor: '#852222',
-                    color: 'white'}, width: 1/4, alignSelf: 'center', mt: 1, backgroundColor: 'rgba(171, 36, 36)'}}>
-                    Log in
+                    <Button sx={{
+                        ':hover': {
+                            bgcolor: '#852222',
+                            color: 'white'
+                        }, width: 1 / 4, alignSelf: 'center', mt: 1, backgroundColor: 'rgba(171, 36, 36)'
+                    }}>
+                        Log in
                     </Button>
                     <Typography
-                    endDecorator={<a style={{color: 'rgba(171, 36, 36)' }} href="/sign-up">Sign up</a>}
-                    fontSize="sm"
-                    sx={{ alignSelf: 'center',color: 'white', fontWeight: 'bold', textAlign: 'center' }}
+                        endDecorator={<a style={{color: 'rgba(171, 36, 36)'}} href="/sign-up">Sign up</a>}
+                        fontSize="sm"
+                        sx={{alignSelf: 'center', color: 'white', fontWeight: 'bold', textAlign: 'center'}}
                     >
-                    <span>Don't have an account?</span>
+                        <span>Don't have an account?</span>
                     </Typography>
-                    <Sheet style={{ background: 'transparent'}}
+                    <Sheet style={{background: 'transparent'}}
                            sx={{
                                mx: 'auto',
                                py: 8,
@@ -91,20 +90,20 @@ function LoginSheet() {
                                gap: 0,
                            }}
                     >
-                            <IconButton size="large" sx={{color: 'white'}}>
-                                <FacebookOutlinedIcon/>
-                            </IconButton>
-                            <IconButton size="large" sx={{color: 'white'}}>
-                                <InstagramIcon/>
-                            </IconButton>
-                            <IconButton size="large" sx={{color: 'white'}}>
-                                <WhatsAppIcon/>
-                            </IconButton>
+                        <IconButton size="large" sx={{color: 'white'}}>
+                            <FacebookOutlinedIcon/>
+                        </IconButton>
+                        <IconButton size="large" sx={{color: 'white'}}>
+                            <InstagramIcon/>
+                        </IconButton>
+                        <IconButton size="large" sx={{color: 'white'}}>
+                            <WhatsAppIcon/>
+                        </IconButton>
                     </Sheet>
-                    </Sheet>
-                    </div>
-                    </div>
-                    );
-                }
+                </Sheet>
+            </div>
+        </div>
+    );
+}
 
 export default LoginSheet;
