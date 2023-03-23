bade952baa44e71e44b0218dd8bce246a93fb89b
Piotr Petryka <ppwxp19@gmail.com>
2023-03-23 19:49:41 +0100

    Refactor : Moving logo to new component

---
+++ jobfever/src/components/login/Logo.js
@@ -0,0 +1,11 @@
+import logo from "../../images/logo2.png";
+import React from "react";
+
+function Logo() {
+    return (
+        <div id="gridedBox">
+            <img id="logo" src={logo} />
+        </div>
+    );
+}
+export default Logo
