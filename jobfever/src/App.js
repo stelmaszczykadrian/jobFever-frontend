d77764cc7521eae4c5d338aa278b81a2dc2e2db3
reizergrzegorz <reizergrzegorz@gmail.com>
2023-03-23 13:29:12 +0100

    Feature: Register_Page

--- jobfever/src/App.js
+++ jobfever/src/App.js
@@ -3,11 +3,12 @@ import WelcomePage from "./components/welcomePage/WelcomePage";
 import ResponsiveAppBar from "./components/navbar";
 import FilterBar from "./components/filter-bar";
 import LoginSheet from './components/login/LoginSheet';
+import Register from "./components/login/Register";
 
 function App() {
   return (
     <div>
-      <WelcomePage />
+      <Register />
     </div>
   );
 }
---
+++ jobfever/src/components/login/Register.js
@@ -0,0 +1,110 @@
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Button from '@mui/joy/Button';
+import Typography from '@mui/joy/Typography';
+import Sheet from '@mui/joy/Sheet';
+import './LoginSheet.css';
+import React from "react";
+import logo from "../../images/logo2.png";
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import IconButton from '@mui/material/IconButton';
+
+function Register() {
+    return (
+        <div>
+            <div id="container" >
+                <div id="flexedBox"></div>
+                <Sheet style={{ background: 'rgba(29, 25, 23, 0.7)'}}
+                       sx={{
+                           width: 1/2,
+                           mx: 'auto', // margin left & right
+                           my: 0, // margin top & botom
+                           py: 20, // padding top & bottom
+                           px: 4, // padding left & right
+                           display: 'flex',
+                           flexDirection: 'column',
+                           gap: 5,
+                       }}
+                >
+                    <div>
+                        <div id="gridedBox">
+                            <img id="logo" src={logo}/>
+                        </div>
+                        <Typography level="h4" component="h1" sx={{ color: 'rgba(171, 36, 36)', fontWeight: 'bold', textAlign: 'center' }}>
+                            <span>Welcome!</span>
+                        </Typography>
+                        <Typography level="body2" component="h1" sx={{ color: 'white', fontWeight: 'bold', textAlign: 'center' }}>
+                            <span>Create account</span>
+                        </Typography>
+                    </div>
+                    <p></p>
+                    <FormControl width="40">
+                        <FormLabel sx={{alignSelf: 'center',color: 'white'}}>Email</FormLabel>
+                        <Input
+                            // html input attribute
+                            error
+                            id="outlined-error-helper-text"
+                            sx={{border: '1px', width: 2/5, alignSelf: 'center', color: 'grey'}}
+                            name="email"
+                            type="email"
+                            placeholder="jobFever@email.com"
+                        />
+                    </FormControl>
+                    <FormControl id="inputLighter">
+                        <FormLabel sx={{border: '1px', alignSelf: 'center', color: 'white'}}>Password</FormLabel>
+                        <Input
+                            error
+                            id="outlined-error-helper-text"
+                            sx={{width: 2/5, alignSelf: 'center', color: 'grey'}}
+                            id="outlined-error"
+                            name="password"
+                            type="password"
+                            placeholder="password"
+                        />
+                    </FormControl>
+                    <FormControl id="inputLighter">
+                        <FormLabel sx={{border: '1px', alignSelf: 'center', color: 'white'}}>Repeat Password</FormLabel>
+                        <Input
+                            error
+                            id="outlined-error-helper-text"
+                            sx={{width: 2/5, alignSelf: 'center', color: 'grey'}}
+                            id="outlined-error"
+                            name="repeat-password"
+                            type="repeat-password"
+                            placeholder="repeat password"
+                        />
+                    </FormControl>
+                    <Button sx={{ ':hover': {
+                            bgcolor: '#852222',
+                            color: 'white'}, width: 1/4, alignSelf: 'center', mt: 1, backgroundColor: 'rgba(171, 36, 36)'}}>
+                        Register
+                    </Button>
+                    <Sheet style={{ background: 'transparent'}}
+                           sx={{
+                               mx: 'auto',
+                               py: 8,
+                               display: 'flex',
+                               flexDirection: 'row',
+                               gap: 0,
+                           }}
+                    >
+                        <IconButton size="large" sx={{color: 'white'}}>
+                            <FacebookOutlinedIcon/>
+                        </IconButton>
+                        <IconButton size="large" sx={{color: 'white'}}>
+                            <InstagramIcon/>
+                        </IconButton>
+                        <IconButton size="large" sx={{color: 'white'}}>
+                            <WhatsAppIcon/>
+                        </IconButton>
+                    </Sheet>
+                </Sheet>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
