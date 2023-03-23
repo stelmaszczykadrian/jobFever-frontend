08f61c375f9da4c401ce777c80ee7252af12d9be
Piotr Petryka <ppwxp19@gmail.com>
2023-03-23 19:54:08 +0100

    Refactor : Moving Social media buttons to new component

---
+++ jobfever/src/components/login/SocialmediaButtons.js
@@ -0,0 +1,33 @@
+import Sheet from "@mui/joy/Sheet";
+import IconButton from "@mui/material/IconButton";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import React from "react";
+
+
+function SocialmediaButtons(){
+    return (
+        <Sheet style={{background: 'transparent'}}
+               sx={{
+                   mx: 'auto',
+                   py: 8,
+                   display: 'flex',
+                   flexDirection: 'row',
+                   gap: 0,
+               }}
+        >
+            <IconButton size="large" sx={{color: 'white'}}>
+                <FacebookOutlinedIcon/>
+            </IconButton>
+            <IconButton size="large" sx={{color: 'white'}}>
+                <InstagramIcon/>
+            </IconButton>
+            <IconButton size="large" sx={{color: 'white'}}>
+                <WhatsAppIcon/>
+            </IconButton>
+        </Sheet>
+    );
+
+}
+export default SocialmediaButtons
