9a0a8fbaefd83bea70c40096db5ad46e98202c45
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-03-23 10:25:29 +0100

    Fix: Delete headers from app.js, update json files

--- jobfever/src/App.js
+++ jobfever/src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import WelcomePage from "./components/welcomePage/WelcomePage";
 import ResponsiveAppBar from "./components/navbar";
-import  ResponsiveAppBar from "./components/navbar";
 import FilterBar from "./components/filter-bar";
-import React from "react";
 import LoginSheet from './components/login/LoginSheet';
 
 function App() {
   return (
     <div>
-      <header>
-          <WelcomePage />
-      </header>
+      <WelcomePage />
     </div>
   );
 }
