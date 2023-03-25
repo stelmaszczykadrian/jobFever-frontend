3f6ac5d3eef10fde933c6b9e57b2696a903f60ad
Piotr Petryka <ppwxp19@gmail.com>
2023-03-25 16:38:14 +0100

    Refactor: Adding working input style component

---
+++ jobfever/src/components/login/UserInputValidationStyleComponent.jsx
@@ -0,0 +1,11 @@
+import Input from "@mui/joy/Input";
+import React from "react";
+import {styled} from "@mui/material";
+
+
+export const UserInputValidationStyleComponent = styled("div")`
+  border: 1px;
+      align-self: center;
+  color: black;
+  margin-top: 2%;
+`;
