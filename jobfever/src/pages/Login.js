ab9ba71611ab3c0d4d7e6588c50f7a6f13f576b9
Piotr Petryka <ppwxp19@gmail.com>
2023-03-26 18:54:52 +0200

    Fix: after rebase

--- jobfever/src/pages/Login.js
+++ jobfever/src/pages/Login.js
@@ -1,100 +1,144 @@
 import Sheet from "@mui/joy/Sheet";
-import logo from "../images/logo2.png";
 import Typography from "@mui/joy/Typography";
 import FormControl from "@mui/joy/FormControl";
-import FormLabel from "@mui/joy/FormLabel";
-import Input from "@mui/joy/Input";
 import RedButton from "../components/buttons/RedButton";
-import IconButton from "@mui/material/IconButton";
-import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import '../css/LoginPage.css';
+import React, {useState} from "react";
+import axiosFetch from "../components/fetch/axiosFetch";
+import {StyledText} from "../components/styled-components/StyledText";
+import Logo from "../components/logo/Logo";
+import {StyledLabel} from "../components/styled-components/StyledLabel";
+import {StyledUserInputValidation} from "../components/styled-components/StyledUserInputValidation";
+import UsernameInput from "../components/inputs/UsernameInput";
+import PasswordInput from "../components/inputs/PasswordInput";
+import SocialmediaButtons from "../components/buttons/SocialmediaButtons";
+import {StyledLeftContainer} from "../components/styled-components/StyledLeftContainer";
+import {RightContainer} from "../components/styled-components/StyledRightConatiner";
 
 function LoginSheet() {
+    const [input, setInput] = useState({
+        username: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const [error, setError] = useState({
+        username: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const onInputChange = e => {
+        const {name, value} = e.target;
+        setInput(prev => ({
+            ...prev,
+            [name]: value
+        }));
+        validateInput(e);
+    }
+
+    const validateInput = e => {
+        let {name, value} = e.target;
+        setError(prev => {
+            const stateObj = {...prev, [name]: ""};
+
+            switch (name) {
+                case "username":
+                    if (!value) {
+                        stateObj[name] = "Please enter Username.";
+                    }
+                    break;
+                case "password":
+                    if (!value) {
+                        stateObj[name] = "Please enter Password.";
+                    } else if (input.confirmPassword && value !== input.confirmPassword) {
+                        stateObj["confirmPassword"] = "Password and Confirm Password does not match.";
+                    } else {
+                        stateObj["confirmPassword"] = input.confirmPassword ? "" : error.confirmPassword;
+                    }
+                    break;
+                case "confirmPassword":
+                    if (!value) {
+                        stateObj[name] = "Please enter Confirm Password.";
+                    } else if (input.password && value !== input.password) {
+                        stateObj[name] = "Password and Confirm Password does not match.";
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return stateObj;
+        });
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const userData = {
+            email: input.username,
+            password: input.password,
+        };
+        axiosFetch(userData, "http://localhost:8080/api/candidates/login");
+    };
+
     return (
         <div>
             <div id="container">
-                <div id="flexedBox"></div>
-                <Sheet style={{background: 'rgba(29, 25, 23, 0.7)'}}
-                       sx={{
-                           width: 1 / 2,
-                           mx: 'auto', // margin left & right
-                           my: 0, // margin top & botom
-                           py: 3, // padding top & bottom
-                           px: 4, // padding left & right
-                           display: 'flex',
-                           flexDirection: 'column',
-                           gap: 2,
-                       }}
-                >
-                    <div>
-                        <div id="gridedBox">
-                            <img id="logo" src={logo}/>
+                <StyledLeftContainer/>
+                <RightContainer>
+                    <Sheet style={{backgroundColor: 'transparent'}}>
+                        <div>
+                            <Logo/>
+                            <StyledText>
+                                <Typography level="h4" component="h1"
+                                            sx={{color: 'rgba(171, 36, 36)'}}>
+                                    <span>Welcome!</span>
+                                </Typography>
+                            </StyledText>
+                            <StyledText>
+                                <Typography level="body1" component="h1"
+                                            sx={{color: 'white'}}>
+                                    <span>Log in</span>
+                                </Typography>
+                            </StyledText>
                         </div>
-                        <Typography level="h4" component="h1"
-                                    sx={{color: 'rgba(171, 36, 36)', fontWeight: 'bold', textAlign: 'center'}}>
-                            <span>Welcome!</span>
-                        </Typography>
-                        <Typography level="body2" component="h1"
-                                    sx={{color: 'white', fontWeight: 'bold', textAlign: 'center'}}>
-                            <span>Sign in to continue.</span>
-                        </Typography>
-                    </div>
-                    <p></p>
-                    <FormControl width="40">
-                        <FormLabel sx={{alignSelf: 'center', color: 'white'}}>Email</FormLabel>
-                        <Input
-                            sx={{border: '1px', width: 2 / 5, alignSelf: 'center', color: 'grey'}}
-                            name="email"
-                            type="email"
-                            placeholder="jobFever@email.com"
-                        />
-                    </FormControl>
-                    <FormControl id="inputLighter">
-                        <FormLabel sx={{border: '1px', alignSelf: 'center', color: 'white'}}>Password</FormLabel>
-                        <Input
-                            sx={{width: 2 / 5, alignSelf: 'center', color: 'grey'}}
-                            name="password"
-                            type="password"
-                            placeholder="password"
-                        />
-                        <Typography
-                            endDecorator={<a style={{color: 'white'}} href="">Forgot a password?</a>}
-                            fontSize="sm"
-                            sx={{color: 'white', fontWeight: 'bold', textAlign: 'center', alignSelf: 'center'}}
-                        ></Typography>
-                    </FormControl>
-                    <RedButton
-                        text={'LOG IN'}>
-                    </RedButton>
-                    <Typography
-                        endDecorator={<a style={{color: 'rgba(171, 36, 36)'}} href="/sign-up">Sign up</a>}
-                        fontSize="sm"
-                        sx={{alignSelf: 'center', color: 'white', fontWeight: 'bold', textAlign: 'center'}}
-                    >
-                        <span>Don't have an account?</span>
-                    </Typography>
-                    <Sheet style={{background: 'transparent'}}
-                           sx={{
-                               mx: 'auto',
-                               py: 8,
-                               display: 'flex',
-                               flexDirection: 'row',
-                               gap: 0,
-                           }}
-                    >
-                        <IconButton size="large" sx={{color: 'white'}}>
-                            <FacebookOutlinedIcon/>
-                        </IconButton>
-                        <IconButton size="large" sx={{color: 'white'}}>
-                            <InstagramIcon/>
-                        </IconButton>
-                        <IconButton size="large" sx={{color: 'white'}}>
-                            <WhatsAppIcon/>
-                        </IconButton>
+                        <p></p>
+                        <form onSubmit={handleSubmit}>
+                            <FormControl width="40">
+                                <StyledLabel>
+                                    <span>E-mail</span>
+                                </StyledLabel>
+                                <StyledUserInputValidation>
+                                    <UsernameInput value={input.username}
+                                                   onChange={onInputChange}
+                                                   onBlur={validateInput}/>
+                                </StyledUserInputValidation>
+                                {error.username && <span className='err'>{error.username}</span>}
+                            </FormControl>
+                            <FormControl>
+                                <StyledLabel>
+                                    <span>Password</span>
+                                </StyledLabel>
+                                <StyledUserInputValidation>
+                                    <PasswordInput name={'password'} value={input.password}
+                                                   onChange={onInputChange}
+                                                   onBlur={validateInput}
+                                    />
+                                </StyledUserInputValidation>
+                                {error.password && <span className='err'>{error.password}</span>}
+                            </FormControl>
+                            <FormControl sx={{justifyContent: 'center'}}>
+                                <RedButton
+                                    text={'LOGIN'}>
+                                </RedButton>
+                            </FormControl>
+                        </form>
+
                     </Sheet>
-                </Sheet>
+                    <SocialmediaButtons/>
+                </RightContainer>
+
             </div>
         </div>
     );
