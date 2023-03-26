b1adb9dd5dcba7bd9799276a7688c18bded46c1a
Piotr Petryka <ppwxp19@gmail.com>
2023-03-26 13:44:34 +0200

    Refactor: changing styled component filenames

---
+++ jobfever/src/components/login/styled-components/StyledLeftContainer.jsx
@@ -0,0 +1,9 @@
+import {styled} from "@mui/material";
+
+export const StyledLeftContainer = styled("div")`
+      flex-basis: 50%;
+      height: 100%;
+      display: grid;
+      align-items: center;
+      justify-content: center;
+    `;
