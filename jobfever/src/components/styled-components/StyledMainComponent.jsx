a205093c45bd8b60231a464a2c38d607cf30734b
Piotr Petryka <ppwxp19@gmail.com>
2023-03-28 18:46:02 +0200

    Feature: Job offer grid prepared

--- jobfever/src/components/styled-components/StyledMainComponent.jsx
+++ jobfever/src/components/styled-components/StyledMainComponent.jsx
@@ -4,10 +4,17 @@ import img from "../../images/background_1.jpg";
 
 export const StyledMainComponent = styled.div`
   display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
   width: 100%;
   height: 100vh;
+  align-items: center;
+  
   background-image: url(${img});
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
+
+  
+
 `;
--- jobfever/src/pages/JobsPage.js
+++ jobfever/src/pages/JobsPage.js
@@ -2,24 +2,33 @@ import React from "react";
 import {StyledMainComponent} from "../components/styled-components/StyledMainComponent";
 import {StyledSortButtonsContainer} from "../components/styled-components/StyledSortButtonsContainer";
 import {SortButton} from "../components/buttons/SortButton";
+import {StyledJobOffersContainer} from "../components/styled-components/StyledJobOffersContainer";
+import JobOfferGrid from "../components/containers/JobOfferGrid";
 
 
-
-
-
-
-export default function JobsPage(){
+export default function JobsPage() {
     return (
         <div>
-        <StyledMainComponent>
-            <StyledSortButtonsContainer>
-            <SortButton>SQL</SortButton>
-            <SortButton>JS</SortButton>
-            <SortButton>C++</SortButton>
-            <SortButton>JAVA</SortButton>
-            <SortButton>Java Developer</SortButton>
-            </StyledSortButtonsContainer>
-        </StyledMainComponent>
+            <StyledMainComponent>
+                <StyledSortButtonsContainer>
+                    <SortButton>SQL</SortButton>
+                    <SortButton>JS</SortButton>
+                    <SortButton>C++</SortButton>
+                    <SortButton>JAVA</SortButton>
+                    <SortButton>Java Developer</SortButton>
+
+                </StyledSortButtonsContainer>
+                <StyledJobOffersContainer>
+                    <JobOfferGrid>
+
+                </JobOfferGrid>
+                    <JobOfferGrid>
+
+                    </JobOfferGrid>
+
+                </StyledJobOffersContainer>
+
+            </StyledMainComponent>
         </div>
     )
 }
