04be5e7739d01416424385b1a70240d11b11f1bc
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-03-29 13:57:40 +0200

    Feature: Added job offer creation form

---
+++ jobfever/src/components/styled-components/StyledInputJobOfferContaine.jsx
@@ -0,0 +1,5 @@
+import {styled} from "@mui/material";
+
+export const StyledInputJobOfferContainer = styled('div')`
+  width: 100%;
+`
--- jobfever/src/components/styled-components/StyledUserInputValidation.jsx
+++ jobfever/src/components/styled-components/StyledUserInputValidation.jsx
@@ -3,7 +3,7 @@ import {styled} from "@mui/material";
 
 
 export const StyledUserInputValidation = styled("div")`
-  border: 1px;
+  border: 5px;
       align-self: center;
   color: black;
   margin-top: 2%;
