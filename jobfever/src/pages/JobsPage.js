8b1f4ab5c1105681dda3b4ae51c224aeaf47444d
Piotr Petryka <ppwxp19@gmail.com>
2023-03-30 15:59:47 +0200

    Refactor: sort btn container change to atomic design pattern

--- jobfever/src/pages/JobsPage.js
+++ jobfever/src/pages/JobsPage.js
@@ -1,23 +1,16 @@
 import React from "react";
 import {StyledMainComponent} from "../components/styled-components/StyledMainComponent";
-import {StyledSortButtonsContainer} from "../components/styled-components/StyledSortButtonsContainer";
 import {SortButton} from "../components/atoms/SortButton";
 import {StyledJobOffersContainer} from "../components/styled-components/StyledJobOffersContainer";
 import JobOfferGrid from "../components/molecules/JobOfferGrid";
+import SortButtonsContainer from "../components/molecules/SortButtonsContainer";
 
 
 export default function JobsPage() {
     return (
         <div>
             <StyledMainComponent>
-                <StyledSortButtonsContainer>
-                    <SortButton>SQL</SortButton>
-                    <SortButton>JS</SortButton>
-                    <SortButton>C++</SortButton>
-                    <SortButton>JAVA</SortButton>
-                    <SortButton>Java Developer</SortButton>
-
-                </StyledSortButtonsContainer>
+                <SortButtonsContainer />
                 <StyledJobOffersContainer>
                     <JobOfferGrid>
 
