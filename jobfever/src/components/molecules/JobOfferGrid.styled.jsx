ccc457ae1332f2a22d79c7bce079e92058848bc1
Piotr Petryka <ppwxp19@gmail.com>
2023-03-30 17:14:28 +0200

    Refactor: job page prearranged to atomic design pattern

---
+++ jobfever/src/components/molecules/JobOfferGrid.styled.jsx
@@ -0,0 +1,15 @@
+import {Paper, styled} from "@mui/material";
+
+
+export const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+});
+export const StyledPaper = styled(Paper)`
+    padding: 2px;
+    margin: auto;
+    margin-bottom: 10px;
+    flex-grow: 1;
+`;
--- jobfever/src/components/organisms/JobsPageMainComponent.styled.jsx
+++ jobfever/src/components/organisms/JobsPageMainComponent.styled.jsx
@@ -2,19 +2,15 @@ import styled from "styled-components";
 import img from "../../images/background_1.jpg";
 
 
-export const StyledMainComponent = styled.div`
+export const StyledJobsPageMainComponent = styled.div`
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
   width: 100%;
   height: 100vh;
   align-items: center;
-  
   background-image: url(${img});
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
-
-  
-
 `;
