49f6acd5663a28bf218035ce8e81c64abc3730d8
stelmaszczykadrian <Tooyota12345@>
2023-03-30 19:01:17 +0200

    Refactor: Refactor Job Offer form to atomic design pattern

---
+++ jobfever/src/pages/JobOfferForm.js
@@ -0,0 +1,11 @@
+import JobOfferFormContainer from "../components/organisms/JobOfferFormContainer";
+
+function JobOfferForm() {
+    return (
+        <JobOfferFormContainer/>
+    );
+}
+
+
+export default JobOfferForm;
+
