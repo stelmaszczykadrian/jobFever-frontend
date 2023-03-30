bcea53419aae672c774613375920b4541379718c
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-03-30 19:01:17 +0200

    Refactor: Refactor Job Offer form to atomic design pattern

---
+++ jobfever/src/components/organisms/JobOfferFormContainer.styles.jsx
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const StyledJobOfferCreationContainer = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  width: 40%;
+  margin: 0 auto;
+  
+
+`;
+
+export const StyledInputJobOfferContainer = styled('div')`
+  width: 100%;
+  
+`
+
+
+export const StyledTextarea = styled("textarea")`
+  width: 97%;
+  height: 150px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+`
+
+export const StyledGridContainer = styled("div")`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  display: grid;
+  grid-template-columns: auto auto;
+  
+`
+export const StyledGridItem = styled("div")`
+  text-align: center;
+  
+`
+
+
+export const StyledSelectJobType = styled("select")`
+  font-size: 1.2rem;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  margin-bottom: 20px;
+  margin-top: 20px;
+  
+`
+
+export const StyledCurrencyType = styled("select")`
+  font-size: 0.8rem;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  
+`
+
+
+export const StyledInputLabel = styled("div")`
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`
+
+export const StyledButtonCenter = styled("div")`
+  text-align: center;
+`
