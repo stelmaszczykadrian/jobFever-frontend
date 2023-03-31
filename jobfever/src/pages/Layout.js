f431906fa2ec97c494fa4818675cca41331f47f6
Piotr Petryka <ppwxp19@gmail.com>
2023-03-31 15:53:51 +0200

    Refactor: Changing axiosfetch name to appropriate http request method

--- jobfever/src/pages/Layout.js
+++ jobfever/src/pages/Layout.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Footer from "../components/molecules/footer";
+import Navbar from "../components/molecules/navbar";
 function Layout(props) {
     return (
         <div>
+            <Navbar />
             <main>{props.children}</main>
             <Footer/>
         </div>
