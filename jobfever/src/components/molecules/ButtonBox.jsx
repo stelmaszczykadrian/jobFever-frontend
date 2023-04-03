05ae6f263c36137461d6df98d1d863b11004acba
MonikaMarkulis <markulismonika@gmail.com>
2023-04-03 16:01:53 +0200

    ButtonBox.jsx refactor

--- jobfever/src/components/molecules/ButtonBox.jsx
+++ jobfever/src/components/molecules/ButtonBox.jsx
@@ -18,27 +18,27 @@ import PowerShell from "../../languages/powershell.svg";
 import Data from "../../languages/data.svg";
 import SQL from "../../languages/sql.svg";
 
+const languageIcons = [
+    <img src={Python} alt="my" width={"30px"}/>,
+    <img src={Ruby} alt="my" width={"30px"}/>,
+    <img src={JS} alt="my" width={"30px"}/>,
+    <img src={C} alt="my" width={"30px"}/>,
+    <img src={CSharp} alt="my" width={"30px"}/>,
+    <img src={PHP} alt="my" width={"30px"}/>,
+    <img src={Kotlin} alt="my" width={"30px"}/>,
+    <img src={Java} alt="my" width={"30px"}/>,
+    <img src={Scala} alt="my" width={"30px"}/>,
+    <img src={Pascal} alt="my" width={"30px"}/>,
+    <img src={Swift} alt="my" width={"30px"}/>,
+    <img src={Dotnet} alt="my" width={"30px"}/>,
+    <img src={PowerShell} alt="my" width={"30px"}/>,
+    <img src={Data} alt="my" width={"30px"}/>,
+    <img src={SQL} alt="my" width={"30px"}/>,
+];
+
+const languageLabels = ['Python', 'Ruby', 'JS', 'C++', 'C#', 'PHP', 'Kotlin', 'Java', 'Scala', 'Pascal', 'Swift', '.Net', 'PowerShell', 'Data', 'SQL'];
+
 export default function ButtonBox(){
-
-    const languageIcons = [
-        <img src={Python} alt="my" width={"30px"}/>,
-        <img src={Ruby} alt="my" width={"30px"}/>,
-        <img src={JS} alt="my" width={"30px"}/>,
-        <img src={C} alt="my" width={"30px"}/>,
-        <img src={CSharp} alt="my" width={"30px"}/>,
-        <img src={PHP} alt="my" width={"30px"}/>,
-        <img src={Kotlin} alt="my" width={"30px"}/>,
-        <img src={Java} alt="my" width={"30px"}/>,
-        <img src={Scala} alt="my" width={"30px"}/>,
-        <img src={Pascal} alt="my" width={"30px"}/>,
-        <img src={Swift} alt="my" width={"30px"}/>,
-        <img src={Dotnet} alt="my" width={"30px"}/>,
-        <img src={PowerShell} alt="my" width={"30px"}/>,
-        <img src={Data} alt="my" width={"30px"}/>,
-        <img src={SQL} alt="my" width={"30px"}/>,
-    ];
-
-    const languageLabels = ['Python', 'Ruby', 'JS', 'C++', 'C#', 'PHP', 'Kotlin', 'Java', 'Scala', 'Pascal', 'Swift', '.Net', 'PowerShell', 'Data', 'SQL'];
 
     return (
         <StyledBox style={{background: 'transparent'}}>
