a83783955334f1bb2d4ee134cbefe952c18fbe85
Piotr Petryka <ppwxp19@gmail.com>
2023-04-04 01:50:59 +0200

    Feature: temporary pagination with button

--- jobfever/src/components/organisms/JobOffersContainer.jsx
+++ jobfever/src/components/organisms/JobOffersContainer.jsx
@@ -2,6 +2,7 @@ import JobOfferGrid from "../molecules/JobOfferGrid";
 import React, {useState, useRef, useCallback} from "react";
 import {StyledJobOffersContainer} from "./JobOffersContainer.styles";
 import {useAxiosPagination} from "../../api/axiosFetch";
+import RedButton from "../atoms/RedButton";
 
 
 export default function JobOffersContainer() {
@@ -14,22 +15,24 @@ export default function JobOffersContainer() {
         error
     } = useAxiosPagination(pageNumber)
 
-    const observer = useRef()
-    let lastJobElementRef = useCallback(node => {
-        if (loading) return
-        if (observer.current) {
-            console.log("dupa")
-            observer.current.disconnect()
-        }
-        observer.current = new IntersectionObserver(entries => {
-            if (entries[0].isIntersecting && hasMore) {
-                setPageNumber(prevPageNumber => prevPageNumber + 1)
-            }
-        })
-        if (node) observer.current.observe(node)
-    }, [loading, hasMore])
+    // const observer = useRef()
+    // let lastJobElementRef = useCallback(node => {
+    //     if (loading) return
+    //     if (observer.current) {
+    //         console.log("dupa")
+    //         observer.current.disconnect()
+    //     }
+    //     observer.current = new IntersectionObserver(entries => {
+    //         if (entries[0].isIntersecting && hasMore) {
+    //             setPageNumber(prevPageNumber => prevPageNumber + 1)
+    //         }
+    //     })
+    //     if (node) observer.current.observe(node)
+    // }, [loading, hasMore])
 
-
+    function onClick(){
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+    }
 
 
 
@@ -39,14 +42,17 @@ export default function JobOffersContainer() {
                 jobs.content?.map((job, index) => {
                     if (jobs.content.length === index + 1) {
                         console.log("jestem")
-                        return (<div key={index} ref={lastJobElementRef}><JobOfferGrid job={job}/></div>)
+                        return (<div key={index} ><JobOfferGrid job={job}/></div>)
                     } else {
                         return (<JobOfferGrid job={job} key={index}/>)
                     }
                 })
             }
+
                 <div>{loading && 'loading'}<div></div><div></div><div></div></div>
                 <div>{error && 'error when loading jobs'}</div>
+
+            <div onClick={onClick} hidden={!hasMore}><RedButton text={"show more"}></RedButton></div>
                 </StyledJobOffersContainer>
                 )
             }
