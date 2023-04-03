50cde1a3c9413163d7f4342da84a593bac6708da
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-04-04 00:24:45 +0200

    Feature: Add profile photo and reusable title of container

---
+++ jobfever/src/components/atoms/ProfileContainerTitle.styles.jsx
@@ -0,0 +1,7 @@
+import {styled} from "@mui/material";
+
+export const StyledProfileContainerTitle = styled("span")`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
---
+++ jobfever/src/components/atoms/ProfilePhoto.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {StyledProfilePhoto} from "./ProfilePhoto.styles";
+
+function ProfilePhoto() {
+    return (
+        <StyledProfilePhoto src="https://i.pravatar.cc/150?img=6" alt="Profile" />
+    );
+}
+export default ProfilePhoto
