d2069892c8b93fd8591dc46bccd6bb374e405f90
Piotr Petryka <ppwxp19@gmail.com>
2023-04-03 18:42:43 +0200

    Pagination test

--- jobfever/src/api/axiosFetch.js
+++ jobfever/src/api/axiosFetch.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {useEffect, useState} from "react";
 
 export default function axiosPost(userData, url) {
     axios
@@ -17,3 +18,33 @@ export default function axiosPost(userData, url) {
             }
         });
 }
+// export function useAxiosPagination(pageNumber){
+//    const [loading, setLoading] = useState(true)
+//    const [error, setError] = useState(false)
+//     const [jobs, setJobs] = useState([])
+//     const [hasMore, setHasMore] = useState(false)
+//
+//     useEffect(() => {
+//         setLoading(true)
+//         setError(false)
+//         let cancel
+//         axios({
+//             method: 'GET',
+//             url: 'http://localhost:8080/api/jobs/pagination',
+//             params: {page: pageNumber},
+//             cancelToken: new axios.CancelToken(c => cancel = c)
+//         }).then(res => {
+//             setJobs(prevJobs => {
+//                 return [...new Set([...prevJobs, ...res.data.docs.map(b => b.title)])]
+//             })
+//             setHasMore(res.data.docs.length > 0)
+//             setLoading(false)
+//             console.log(res.data)
+//         }).catch(e => {
+//             if (axios.isCancel(e)) return
+//             setError(true)
+//         })
+//     }, [pageNumber])
+//         return {loading,error,jobs,hasMore}
+//
+// }
