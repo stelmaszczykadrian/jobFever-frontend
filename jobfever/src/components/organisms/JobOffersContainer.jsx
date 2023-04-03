fb950d9081b25372d80b99c07b427d1ba31adc48
Piotr Petryka <ppwxp19@gmail.com>
2023-04-03 18:42:43 +0200

    Pagination test

--- jobfever/src/components/organisms/JobOffersContainer.jsx
+++ jobfever/src/components/organisms/JobOffersContainer.jsx
@@ -1,15 +1,26 @@
 import JobOfferGrid from "../molecules/JobOfferGrid";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {StyledJobOffersContainer} from "./JobOffersContainer.styles";
-
+import axios from "axios";
+import {Pagination} from "@mui/material";
 
 
 export default function JobOffersContainer(){
 
+
+
+
+    const res = axios.get('http://localhost:8080/api/jobs/pagination');
+    console.log(res)
+
+
+
+
+
     return (
         <StyledJobOffersContainer>
-            <JobOfferGrid />
-            <JobOfferGrid />
+            {/*<JobOfferGrid />*/}
+            {/*<JobOfferGrid />*/}
         </StyledJobOffersContainer>
     )
 }
