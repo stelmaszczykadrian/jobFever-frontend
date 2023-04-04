29adff5a75a3361ad82c2f2fe6c151d78b3c7664
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-04-04 13:32:34 +0200

    Fix: Change styled tag to div

--- jobfever/src/components/atoms/ProfileCheckIcon.styles.jsx
+++ jobfever/src/components/atoms/ProfileCheckIcon.styles.jsx
@@ -1,6 +1,6 @@
 import {styled} from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 
-export const StyledProfileCheckIcon = styled(IconButton)`
+export const StyledProfileCheckIcon = styled("div")`
   color: rgb(183, 4, 11);
 `;
