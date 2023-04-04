6b33fcd2f1ecdb0220695384aaee233c4867c578
stelmaszczykadrian <Tooyota12345@>
2023-04-04 15:52:13 +0200

    Feature: Added user existence check on frontend, displays appropriate message if user already exists or not

--- jobfever/src/components/organisms/EmployerRegisterRightContainer.styles.jsx
+++ jobfever/src/components/organisms/EmployerRegisterRightContainer.styles.jsx
@@ -12,3 +12,12 @@ export const StyleEmployerRegisterSubmitButton = styled('div')`
   text-align: center;
 
 `
+
+
+export const EmployerRegisterTextEmployerExist = styled('div')`
+  color: whitesmoke;
+  text-align: center;
+  margin-top: 40%;
+  font-weight: bold;
+  font-size: x-large;
+`
