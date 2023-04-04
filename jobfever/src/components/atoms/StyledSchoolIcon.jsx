cc95a9dd39f93b8d629f6ba56195d018ffad5569
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-04-04 22:14:46 +0200

    Feature: Add school,add and work icons

---
+++ jobfever/src/components/atoms/StyledSchoolIcon.jsx
@@ -0,0 +1,10 @@
+import { School as MUISchoolIcon } from '@mui/icons-material'
+import React from "react";
+import {styled} from "@mui/material";
+
+
+export const StyledSchoolIcon = styled(MUISchoolIcon)(() => ({
+    fontSize: '2em',
+    color: '#666',
+    marginRight: '1rem'
+}))
