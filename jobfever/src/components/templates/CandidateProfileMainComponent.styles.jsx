2cf89116956fa77f279730f713133415273a774d
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-04-04 20:57:02 +0200

    Refactor: Styled input text, improving display of items on page

--- jobfever/src/components/templates/CandidateProfileMainComponent.styles.jsx
+++ jobfever/src/components/templates/CandidateProfileMainComponent.styles.jsx
@@ -1,7 +1,14 @@
 import {styled} from "@mui/material";
+import img from "../../images/background_6.jpg"
+
 
 export const StyledCandidateProfileMainComponent = styled("div")`
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100vh;
-  background-color: darkgrey;
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
 `
