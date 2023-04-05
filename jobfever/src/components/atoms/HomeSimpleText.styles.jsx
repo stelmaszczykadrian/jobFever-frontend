d4d4916ec9c510e5a717ef74dfa7ff1a069ce872
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-04-05 09:30:09 +0200

    Refactor: Transfer HomeHeader and SimpleText from atoms to HomeRightContainer style

--- jobfever/src/components/atoms/HomeSimpleText.styles.jsx
+++
@@ -1,5 +0,0 @@
-import {styled} from "@mui/material";
-
-export const StyledHomeSimpleText = styled("div")`
-    width: 90%;
-    font-size: larger;`
--- jobfever/src/components/organisms/HomeRightContainer.styles.jsx
+++ jobfever/src/components/organisms/HomeRightContainer.styles.jsx
@@ -9,3 +9,11 @@ export const StyledHomeRightContainer = styled("div")`
     background: rgba(100,12,9,0.85);
     grid-template-rows: 2fr 1fr 1fr 1fr;
     `;
+
+export const StyledHomeHeader = styled("div")`
+    width: 90%;
+    font-size: xxx-large;`
+
+export const StyledHomeSimpleText = styled("div")`
+    width: 90%;
+    font-size: larger;`
