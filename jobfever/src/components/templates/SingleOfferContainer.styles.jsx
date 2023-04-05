c935aa47724efd62ce2aae298cbbe5b985418cbf
reizergrzegorz <reizergrzegorz@gmail.com>
2023-04-05 09:23:43 +0200

    Feature: Single Offer Main Container with Styles

---
+++ jobfever/src/components/templates/SingleOfferContainer.styles.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import img from "../images/background_3.jpg"
+
+
+export const StyledSingleOfferContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 1%;
+  width: 100%;
+  height: 100vh;
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  
+`;
