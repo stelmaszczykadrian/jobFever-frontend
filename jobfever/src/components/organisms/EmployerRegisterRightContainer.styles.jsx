4a232133255078efa6ac9b1a1e53085c6df71bd2
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-04-05 16:20:21 +0200

    Fix: employer register button is displaying on center

--- jobfever/src/components/organisms/EmployerRegisterRightContainer.styles.jsx
+++ jobfever/src/components/organisms/EmployerRegisterRightContainer.styles.jsx
@@ -1,18 +1,16 @@
 import styled from "styled-components";
 
-
 export const EmployerRegisterRightContainerColumn = styled('div')`
   float: left;
   width: 50%;
 `
 
-
-
-
 export const StyleEmployerRegisterSubmitButton = styled('div')`
   text-align: center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `
-
 
 export const EmployerRegisterTextEmployerExist = styled('div')`
   color: whitesmoke;
