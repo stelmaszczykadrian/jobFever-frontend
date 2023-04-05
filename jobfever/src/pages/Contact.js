caf5cfc3bfad8a078a7ba6cd6697e22a48681adc
MonikaMarkulis <markulismonika@gmail.com>
2023-04-05 11:25:01 +0200

    Modal form basic version

--- jobfever/src/pages/Contact.js
+++ jobfever/src/pages/Contact.js
@@ -1,7 +1,10 @@
+import ResponsiveDialog from "./AddEducationModal";
+
 function Contact() {
     return (
         <div>
             <h1>This is the CONTACT page</h1>
+            <ResponsiveDialog/>
         </div>
 
     )}
