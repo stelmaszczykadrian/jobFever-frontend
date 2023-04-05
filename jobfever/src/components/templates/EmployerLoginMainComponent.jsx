000c3999d00c28c6931c0f2b661eabf74401f91d
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-04-05 15:07:57 +0200

    Feature: Added employer login functionality.

---
+++ jobfever/src/components/templates/EmployerLoginMainComponent.jsx
@@ -0,0 +1,13 @@
+import {StyledEmployerRegisterMainComponent} from "./EmployersRegisterMainComponent.styles";
+import LeftContainer from "../organisms/LeftContainer";
+import LoginContainer from "../organisms/LoginContainer";
+
+
+export default function EmployerLoginMainComponent({ apiUrl }){
+    return(
+        <StyledEmployerRegisterMainComponent>
+            <LeftContainer/>
+            <LoginContainer apiUrl={apiUrl}/>
+        </StyledEmployerRegisterMainComponent>
+    );
+}
--- jobfever/src/pages/EmployersLogin.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function EmployersLogin(){
-    return(
-        <div>
-            Employers login.
-        </div>
-    );
-}
