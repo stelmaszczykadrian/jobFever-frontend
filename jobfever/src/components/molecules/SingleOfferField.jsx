7b798cef47b9af4ddfac8cf163d1136cce7dc9b0
reizergrzegorz <reizergrzegorz@gmail.com>
2023-04-05 09:33:36 +0200

    Feature: Single Offer Text Field with Styles

---
+++ jobfever/src/components/molecules/SingleOfferField.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {StyledSingleOfferField} from "./SingleOfferField.styles";
+import SingleOfferFieldTitle from "../atoms/SingleOfferFieldTitle";
+import SingleOfferFieldText from "../atoms/SingleOfferFieldText";
+
+
+
+function SingleOfferField(props) {
+    return (
+        <StyledSingleOfferField>
+            <SingleOfferFieldTitle title={props.title}></SingleOfferFieldTitle>
+            <SingleOfferFieldText text={props.text}></SingleOfferFieldText>
+        </StyledSingleOfferField>
+    );
+}
+export default SingleOfferField;
+
---
+++ jobfever/src/components/molecules/SingleOfferField.styles.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledSingleOfferField = styled.div`
+  text-align: left;
+  color: black;
+  background-color: lightgray;
+  border-radius: 0.5rem;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  margin: auto;
+`;
