a71bdd2b378a8af7ff5f53e56851ad8cf176fb8d
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-04-05 18:07:52 +0200

    Fix: displaying background on the whole page

--- jobfever/src/components/templates/CandidateProfileMainComponent.styles.jsx
+++ jobfever/src/components/templates/CandidateProfileMainComponent.styles.jsx
@@ -6,7 +6,7 @@ export const StyledCandidateProfileMainComponent = styled("div")`
   display: flex;
   flex-direction: column;
   width: 100%;
-  //height: 100vh;
+  height: 100vh;
   background-image: url(${img});
   background-repeat: inherit;
   background-position: center;
