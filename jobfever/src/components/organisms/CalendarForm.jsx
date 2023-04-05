6fb6fd61bf4ffb8603f4f7dc2c96216215453aca
MonikaMarkulis <markulismonika@gmail.com>
2023-04-05 14:22:49 +0200

    Modal Form refactor

--- jobfever/src/components/organisms/CalendarForm.jsx
+++ jobfever/src/components/organisms/CalendarForm.jsx
@@ -3,7 +3,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-export default function HelperText() {
+export default function CalendarForm() {
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker
