f30a1ebfe8d7c5b9fabc605ebe53f123300820c6
Piotr Petryka <ppwxp19@gmail.com>
2023-04-06 00:08:21 +0200

    Refactor: moving jobs fetches to separate file

---
+++ jobfever/src/api/apiJobs.js
@@ -0,0 +1,55 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+export function useJobsPagination(pageNumber, sortBy, field) {
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const [jobs, setJobs] = useState([])
+    const [hasMore, setHasMore] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        setError(false)
+        let cancel
+        axios({
+            method: 'GET',
+            url: 'http://localhost:8080/api/jobs/',
+            params: {page: pageNumber - 1, sortBy: sortBy, field: field},
+            cancelToken: new axios.CancelToken(c => cancel = c)
+        }).then(res => {
+            setJobs(prevJobs => {
+                return [...new Set([...prevJobs, ...res.data.content])]
+            })
+            setHasMore(res.data.content.length > 0)
+            setLoading(false)
+        }).catch(e => {
+            if (axios.isCancel(e)) return
+            setError(true)
+        })
+        return () => cancel()
+    }, [pageNumber])
+    return {loading, error, jobs, hasMore}
+}
+export const useJobsByName = (url, id) => {
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { data: response } = await axios.get(url, {params:{id:id}});
+                setData(response.content);
+            } catch (error) {
+                console.error(error)
+            }
+            setLoading(false);
+        };
+
+        fetchData();
+    }, []);
+
+    return {
+        data,
+        loading,
+    };
+};
