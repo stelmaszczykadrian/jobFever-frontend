f7f42606578ba49d8f4bfdff8cfe7c81f3f99d6e
reizergrzegorz <reizergrzegorz@gmail.com>
2023-04-05 15:53:32 +0200

    Feature: Add Single Offer Page

---
+++ jobfever/src/pages/SingleOfferPage.js
@@ -0,0 +1,18 @@
+import SingleOfferMainComponent from "../components/templates/SingleOfferMainComponent";
+
+function SingleOfferPage() {
+
+    return (
+        <SingleOfferMainComponent />
+
+    )}
+
+export default SingleOfferPage;
+
+
+
+
+
+
+
+
