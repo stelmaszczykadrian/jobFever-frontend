795d057b7fb78d5ba9b58c89e1f812bde2ce0fae
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-04-06 14:21:14 +0200

    Feature: Candidate personal information are updated and saved to database

--- jobfever/src/components/molecules/CandidateProfilePersonalInformation.jsx
+++ jobfever/src/components/molecules/CandidateProfilePersonalInformation.jsx
@@ -12,11 +12,12 @@ import IconButton from "@mui/material/IconButton";
 import {StyledEditIcon} from "../atoms/StyledEditIcon";
 import {StyledCheckIcon} from "../atoms/StyledCheckIcon";
 import {StyledPersonIcon} from "../atoms/StyledPersonIcon";
-import {useCandidateById} from "../../api/CandidateApi";
+import {editCandidate, useCandidateById} from "../../api/CandidateApi";
+import Input from "@mui/joy/Input";
 
 function EditableInput({isEdit, value, onChange, placeholder}) {
     if (isEdit) {
-        return <input type="text" placeholder={placeholder} value={value} onChange={onChange}/>;
+        return <Input type="text" placeholder={placeholder} value={value} onChange={onChange}/>;
     } else {
         return <span>{value}</span>;
     }
@@ -28,19 +29,32 @@ export default function CandidateProfilePersonalInformation(props) {
     const [isEdit, setIsEdit] = useState(false);
     const [name, setName] = useState('Name Surname');
     const [city, setCity] = useState('City');
-    const [email, setEmail] = useState('');
     const [linkedin, setLinkedIn] = useState('https://www.linkedin.com/');
     const [github, setGitHub] = useState('https://github.com/');
-    const [cv, setCv] = useState(null);
     const handleEditClick = () => {
         setIsEdit(true);
     };
-    const handleSaveClick = () => {
+    const handleSaveClick = async () => {
         setIsEdit(false);
+        const updatedCandidateData = {
+            name: name,
+            city: city,
+            linkedin: linkedin,
+            github: github
+        };
+        await editCandidate(props.id, updatedCandidateData);
     };
 
+    React.useEffect(() => {
+        if (data) {
+            setName(data.name);
+            setCity(data.city);
+            setLinkedIn(data.linkedin);
+            setGitHub(data.github);
+        }
+    }, [data]);
+
     if (!loading) {
-        console.log(data)
         return (
             <StyledProfilePaper>
                 <StyledTopBox>
@@ -57,7 +71,7 @@ export default function CandidateProfilePersonalInformation(props) {
                         <Box mb={1}>
                             <EditableInput
                                 isEdit={isEdit}
-                                value={data.name}
+                                value={name}
                                 onChange={(e) => setName(e.target.value)}
                                 placeholder="Name Surname"
                             />
@@ -66,7 +80,7 @@ export default function CandidateProfilePersonalInformation(props) {
                         <Box mb={1}>
                             <EditableInput
                                 isEdit={isEdit}
-                                value={data.city}
+                                value={city}
                                 onChange={(e) => setCity(e.target.value)}
                                 placeholder="City"
                             />
@@ -82,7 +96,7 @@ export default function CandidateProfilePersonalInformation(props) {
                         <Box mb={1}>
                             <EditableInput
                                 isEdit={isEdit}
-                                value={data.linkedin}
+                                value={linkedin}
                                 onChange={(e) => setLinkedIn(e.target.value)}
                                 placeholder="LinkedIn profile link"
                             />
@@ -90,7 +104,7 @@ export default function CandidateProfilePersonalInformation(props) {
                         <Box mb={2}>
                             <EditableInput
                                 isEdit={isEdit}
-                                value={data.github}
+                                value={github}
                                 onChange={(e) => setGitHub(e.target.value)}
                                 placeholder="GitHub profile link"
                             />
