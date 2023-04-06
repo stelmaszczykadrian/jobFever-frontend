1220d9d4d7ed27ee00c4fedf2b3aea340a9281c7
reizergrzegorz <reizergrzegorz@gmail.com>
2023-04-06 22:02:45 +0200

    Refactor: Make Single Job Offer Container Able To Display Dynamic Data

--- jobfever/src/components/organisms/SingleOfferLeftContainer.jsx
+++ jobfever/src/components/organisms/SingleOfferLeftContainer.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import {StyledSingleOfferLeftContainer} from "./SingleOfferLeftContainer.styles";
 import SingleOfferField from "../molecules/SingleOfferField";
+import {useGetJobOfferById} from "../../api/JobsApi";
 
-const titles = ['Description', "Technical Requirements", "Responsibilities", "Who We Are Looking For", "Benefits"]
+
+
 
 function SingleOfferLeftContainer() {
+
+    const offerDetails = useGetJobOfferById();
+
     return (
         <StyledSingleOfferLeftContainer>
-            {titles.map((title,index) =>
-                (<SingleOfferField key={`${title}_${index}`} title={title} text="Some Offer Information"/>))}
+            {/*<SingleOfferField title={offerDetails.employer.companyName} text={offerDetails.title} />*/}
+            <SingleOfferField title="Offer Title" text={offerDetails.title} />
+            <SingleOfferField title="Description" text={offerDetails.description} />
+            <SingleOfferField title="Technical Requirements" text={offerDetails.technicalRequirements} />
+            <SingleOfferField title="Responsibilities" text={offerDetails.responsibilities} />
+            <SingleOfferField title="Who We Are Looking For" text={offerDetails.whoWeAreLookingFor} />
+            <SingleOfferField title="Benefits" text={offerDetails.benefits} />
         </StyledSingleOfferLeftContainer>
     );
 }
 export default SingleOfferLeftContainer;
 
+// const titles = ['Description', "Technical Requirements", "Responsibilities", "Who We Are Looking For", "Benefits"]
+{/*{titles.map((title,index) =>*/}
+{/*    (<SingleOfferField key={`${title}_${index}`} title={title} text="Some Offer Information"/>))}*/}
