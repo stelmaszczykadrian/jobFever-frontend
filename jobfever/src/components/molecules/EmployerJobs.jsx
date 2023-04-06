758fc5ccc3059381bbe9f136b506a5029944c941
Piotr Petryka <ppwxp19@gmail.com>
2023-04-06 16:20:08 +0200

    Fix: Employer profile margin between papers

--- jobfever/src/components/molecules/EmployerJobs.jsx
+++ jobfever/src/components/molecules/EmployerJobs.jsx
@@ -12,18 +12,16 @@ export default function EmployerJobs(props) {
     if (!loading) {
         console.log(data)
         return(
-        <div>
+
             <StyledProfilePaper>
                 <ProfileContainerTitle text={'Our Jobs'}/>
-            </StyledProfilePaper>
-            <StyledProfilePaper>
                 {
                     data.map((job, index) => {
                         return (<JobOfferGrid job={job} key={index}/>)
                     })
                 }
             </StyledProfilePaper>
-        </div>)
+)
     }
     return <span>Loading...</span>;
 }
