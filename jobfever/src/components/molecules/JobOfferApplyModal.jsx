139fc215635272bccb7febdb29377cf0bfc1daa1
MonikaMarkulis <markulismonika@gmail.com>
2023-04-19 14:37:37 +0200

    Feature: Add Modal to apply, Add searching bar

--- jobfever/src/components/molecules/JobOfferApplyModal.jsx
+++ jobfever/src/components/molecules/JobOfferApplyModal.jsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
+import axios from "axios";
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Input from "@mui/joy/Input";
-import {useState} from "react";
+
 import {StyledGridContainer, StyledGridItem} from "../organisms/JobOfferFormContainer.styles";
 import {RedButtonStyled} from "../atoms/RedButton.styles";
 import CalendarForm from "../organisms/CalendarForm";
@@ -14,21 +15,44 @@ import {editCandidateEducation, addCandidateEducation} from "../../api/Candidate
 import RedButton from "../atoms/RedButton";
 import {Checkbox} from "@mui/joy";
 import {FormControlLabel} from "@mui/material";
+import StyledText from "../atoms/StyledText";
 
 export default function JobOfferApplyModal() {
 
-    const title = "Apply for this offer";
-
-    let userDetails = JSON.parse(localStorage.getItem('user'));
+    const title = "Are You sure that You wanna apply for this offer?";
 
     const [open, setOpen] = React.useState(false);
     const [fullWidth, setFullWidth] = React.useState(true);
     const [maxWidth, setMaxWidth] = React.useState('sm');
 
+    const [state, setState] = React.useState({
+        r1: false,
+        r2: false,
+    });
 
     const handleClickOpen = () => {
         setOpen(true);
     };
+
+    const handleChange = (event) => {
+        // const error = validateInput(state);
+        setState({
+            ...state,
+            [event.target.name]: event.target.checked,
+            // errors: {...state.errors, [event.target.name]: error}
+        });
+    };
+
+    // const validateInput = (state) => {
+    //     switch (state.r1, state.r2) {
+    //         case state.r1:
+    //             return state.r1 !== false ? '' : 'You have to accept the regulations!';
+    //         case state.r2:
+    //             return state.r2 !== false ? '' : 'You have to accept the regulations!';
+    //         default:
+    //             return '';
+    //     }
+    // };
 
     const handleClose = () => {
         setOpen(false);
@@ -36,7 +60,6 @@ export default function JobOfferApplyModal() {
 
     const handleSave = async () => {
         setOpen(false);
-        console.log(userDetails);
         };
 
     return (
@@ -51,12 +74,32 @@ export default function JobOfferApplyModal() {
                 onClose={handleClose}
                 aria-labelledby="responsive-dialog-title"
             >
-                <DialogTitle id="responsive-dialog-title">
+                <DialogTitle sx={{textAlign : "center"}} id="responsive-dialog-title">
                     {title}
                 </DialogTitle>
                 <DialogContent>
+                    <DialogContentText component="legend" sx={{textAlign : "center"}}> If You're sure, please accept the RODO to continue application process.</DialogContentText>
+                    <p/>
                     <StyledGridItem>
-                        <Checkbox/>
+                        <FormControlLabel control={<Checkbox
+                            color="danger"
+                            onChange={handleChange}
+                            // onBlur={validateInput}
+                            name = "r1"/>}
+                         label="I hereby give consent for my personal data to be processed for the purpose of conducting recruitment for the position for which I am applying."/>
+                    </StyledGridItem>
+                    <p/>
+                    <StyledGridItem>
+                        <FormControlLabel control={<Checkbox color="danger"
+                             onChange={handleChange}
+                             // onBlur={validateInput}
+                             name = "r2"/>}
+                         label="I also consent to processing of my personal data for the purposes of any future recruitment processes."/>
+                        {state.r1.errors && <StyledText
+                            tag="span"
+                            color="red"
+                            text={state.r1.errors}
+                        />}
                     </StyledGridItem>
                 </DialogContent>
                 <DialogActions>
@@ -64,7 +107,7 @@ export default function JobOfferApplyModal() {
                         Discard
                     </RedButtonStyled>
                     <RedButtonStyled onClick={handleSave}>
-                        Save
+                        Apply
                     </RedButtonStyled>
                 </DialogActions>
             </Dialog>
