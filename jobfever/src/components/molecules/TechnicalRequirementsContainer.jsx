e485a7ce9f6f7234d966f1169e44f22fbb707880
MonikaMarkulis <markulismonika@gmail.com>
2023-04-19 20:18:42 +0200

    Fix: Replace searching bar

--- jobfever/src/components/molecules/TechnicalRequirementsContainer.jsx
+++ jobfever/src/components/molecules/TechnicalRequirementsContainer.jsx
@@ -2,10 +2,6 @@ import {StyledContainer} from "./TechnicalRequirementsContainer.styles";
 import * as React from "react";
 import {StyledSortButton} from "../atoms/StyledSortButton";
 import {languageIcons, languageLabels} from "../../constants/constans";
-import {Search} from "@mui/icons-material";
-import SearchIcon from '@mui/icons-material/Search';
-import {InputBase} from "@mui/material";
-import { styled, alpha } from '@mui/material/styles';
 import DialogContentText from "@mui/material/DialogContentText";
 
 const TechnicalRequirementsContainer = ({pressedButtons, setPressedButtons, setInput}) => {
@@ -25,50 +21,6 @@ const TechnicalRequirementsContainer = ({pressedButtons, setPressedButtons, setI
         }));
     }
 
-    const Search = styled('div')(({ theme }) => ({
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-        '&:hover': {
-            backgroundColor: alpha(theme.palette.common.white, 0.35),
-        },
-        marginLeft: 0,
-        marginTop: 6,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
-            width: 'auto',
-        },
-    }));
-
-    const SearchIconWrapper = styled('div')(({ theme }) => ({
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: 'white',
-    }));
-
-    const StyledInputBase = styled(InputBase)(({ theme }) => ({
-        color: 'white',
-        '& .MuiInputBase-input': {
-            padding: theme.spacing(1, 12, 1, 0),
-            // vertical padding + font size from searchIcon
-            paddingLeft: `calc(3em + ${theme.spacing(0)})`,
-            transition: theme.transitions.create('width'),
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                width: '6ch',
-                '&:focus': {
-                    width: '12ch',
-                },
-            },
-        },
-    }));
-
     return (
         <div>
         <StyledContainer>
@@ -86,17 +38,6 @@ const TechnicalRequirementsContainer = ({pressedButtons, setPressedButtons, setI
                         <DialogContentText sx={{color: 'white'}}>{languageLabels[index]}</DialogContentText></div>
                 </div>
             ))}
-            <div>
-            <Search>
-                <SearchIconWrapper>
-                    <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                    placeholder="Search…"
-                    inputProps={{ 'aria-label': 'search' }}
-                />
-            </Search>
-            </div>
         </StyledContainer>
         </div>
     );
