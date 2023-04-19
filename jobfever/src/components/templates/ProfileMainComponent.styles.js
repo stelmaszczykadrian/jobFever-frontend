8ff7fe52a6825e7fb388d77d73351b20eda04a73
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-04-19 11:10:13 +0200

    Fix: Display background on profile pages responsively

--- jobfever/src/components/templates/ProfileMainComponent.styles.js
+++ jobfever/src/components/templates/ProfileMainComponent.styles.js
@@ -6,6 +6,7 @@ export const StyledProfileMainComponent = styled("div")`
   display: flex;
   flex-direction: column;
   width: 100%;
+  min-height: 100vh;
   background-image: url(${img});
   background-repeat: inherit;
   background-position: center;
