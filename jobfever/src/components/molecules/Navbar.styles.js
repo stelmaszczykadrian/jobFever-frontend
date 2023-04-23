daee5115b8d5244c611bbf5d5f05b049e179654c
stelmaszczykadrian <Tooyota12345@>
2023-04-23 11:33:40 +0200

    Refactor: Removed notifications and updated post job button

---
+++ jobfever/src/components/molecules/Navbar.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import Button from "@mui/joy/Button";
+
+
+export const StyledPostJobButton = styled(Button)(() => ({
+    alignSelf: 'center',
+    mt: 5,
+    backgroundColor: 'rgba(171, 36, 36) !important',
+    ':hover': {
+        backgroundColor: '#852222',
+        color: 'white'
+    },
+}));
+
+
