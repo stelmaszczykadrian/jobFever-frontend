b3ebbc473d8862f58bcf1c153f88693e3245b225
stelmaszczykadrian <Tooyota12345@>
2023-04-24 15:01:33 +0200

    Feature: Contact formula send data to backend

--- jobfever/src/components/organisms/ContactPageContainer.jsx
+++ jobfever/src/components/organisms/ContactPageContainer.jsx
@@ -9,8 +9,60 @@ import {
     StyledContactPageTextarea,
     StyledContactPageMainContainer
 } from "./ContactPageContainer.styles";
+import {useState} from "react";
+import axios from "axios";
+import {Form} from "react-bootstrap";
 
 export default function ContactPageContainer() {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
+        errors: {
+            name: '',
+            email: '',
+            phone: '',
+            message: ''
+        }
+    });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
+
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const userData = {
+            name: formData.name,
+            email: formData.email,
+            phone: formData.phone,
+            message: formData.message
+        };
+
+
+        console.log(userData)
+
+
+        axios.post('http://localhost:8080/api/contact', userData)
+            .then(response => {
+                console.log(response.data);
+                // Do something with response data
+            })
+            .catch(error => {
+                console.log(error);
+                // Handle error
+            });
+
+    }
+
     return (
         <StyledContactPageMainContainer>
             <StyledContactPageContainer>
@@ -18,11 +70,13 @@ export default function ContactPageContainer() {
                     <StyledContactPageLeftContainer/>
                     <StyledContactPageRightContainer>
                         <StyledContactPageHeading>Contact Us</StyledContactPageHeading>
-                        <StyledContactPageInputField type="text" placeholder="Your Name"></StyledContactPageInputField>
-                        <StyledContactPageInputField type="text" placeholder="Your Email"></StyledContactPageInputField>
-                        <StyledContactPageInputField type="text" placeholder="Phone"></StyledContactPageInputField>
-                        <StyledContactPageTextarea placeholder="Message"></StyledContactPageTextarea>
-                        <StyledContactPageButton>Send</StyledContactPageButton>
+                        <Form onSubmit={handleSubmit}>
+                            <StyledContactPageInputField type="text" placeholder="Your Name" name="name" value={formData.name} onChange={handleChange}></StyledContactPageInputField>
+                            <StyledContactPageInputField type="text" placeholder="Your Email" name="email" value={formData.email} onChange={handleChange}></StyledContactPageInputField>
+                            <StyledContactPageInputField type="text" placeholder="Phone" name="phone" value={formData.phone} onChange={handleChange}></StyledContactPageInputField>
+                            <StyledContactPageTextarea placeholder="Message" name="message" value={formData.message} onChange={handleChange}></StyledContactPageTextarea>
+                            <StyledContactPageButton type="submit">Send</StyledContactPageButton>
+                        </Form>
                     </StyledContactPageRightContainer>
                 </StyledContactBox>
             </StyledContactPageContainer>
