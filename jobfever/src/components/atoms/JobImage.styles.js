ac61454b70a1718d13a7035aa282fced38a5c03f
Piotr Petryka <ppwxp19@gmail.com>
2023-04-26 16:47:22 +0200

    JOBF-111 - Refactor: styling profile photo

---
+++ jobfever/src/components/atoms/JobImage.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledJobImage = styled("img")`
+  height: 100px;
+  width: 100px;
+  border-radius: 10%;
+  margin-top: 17%;
+  margin-left: 17%;
+`
