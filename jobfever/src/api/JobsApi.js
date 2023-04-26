e1b03a8efd8a576ac79c85c03768abdfa24e1696
reizergrzegorz <reizergrzegorz@gmail.com>
2023-04-26 16:25:01 +0200

    Feature: Add updateJob

--- jobfever/src/api/JobsApi.js
+++ jobfever/src/api/JobsApi.js
@@ -38,6 +38,30 @@ export const useJobsByName = (id) => {
 
 export const getJobOfferById = async (id) => await axios.get(`http://localhost:8080/api/jobs/${id}`);
 
+export const updateJob = (userData, onSuccess, onError, id) => {
+    axios.put(`http://localhost:8080/api/jobs/${id}`, userData)
+        .then((r) => {
+            console.log(r);
+            if(r.status === 200) {
+                onSuccess();
+            }
+        })
+        .catch((error) => {
+            if (error.response) {
+                const errorMessages = error.response.data.split(". ");
+                onError(errorMessages);
+                console.log(error.response);
+                console.log("server responded");
+            } else if (error.request) {
+                console.log("network error");
+            } else {
+                console.log(error);
+            }
+        });
+
+};
+
+
 
 export const createJob = (userData, onSuccess, onError) => {
     const url = "http://localhost:8080/api/jobs";
