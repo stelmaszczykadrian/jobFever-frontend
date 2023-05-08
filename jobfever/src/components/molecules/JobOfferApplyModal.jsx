cc92bcc376173c26a88e140db6d540ffafaff93e
Piotr Petryka <ppwxp19@gmail.com>
2023-05-08 15:55:39 +0200

    JOBF-42 - Fix : rodo modal validation

--- jobfever/src/components/molecules/JobOfferApplyModal.jsx
+++ jobfever/src/components/molecules/JobOfferApplyModal.jsx
@@ -19,35 +19,13 @@ export default function JobOfferApplyModal(props) {
     const [open, setOpen] = useState(false);
     const [fullWidth, setFullWidth] = useState(true);
     const [maxWidth, setMaxWidth] = useState('sm');
-    const [state, setState] = useState({
-        r1: false,
-        r2: false,
-    });
     const [appliedText, setAppliedText] = useState("");
+    const [isFirstBoxChecked, setIsFirstBoxChecked] = useState(false)
+    const [isSecoundBoxChecked, setIsSecoundBoxChecked] = useState(false)
 
     const handleClickOpen = () => {
         setOpen(true);
     };
-
-    const handleChange = (event) => {
-        // const error = validateInput(state);
-        setState({
-            ...state,
-            [event.target.name]: event.target.checked,
-            // errors: {...state.errors, [event.target.name]: error}
-        });
-    };
-
-    // const validateInput = (state) => {
-    //     switch (state.r1, state.r2) {
-    //         case state.r1:
-    //             return state.r1 !== false ? '' : 'You have to accept the regulations!';
-    //         case state.r2:
-    //             return state.r2 !== false ? '' : 'You have to accept the regulations!';
-    //         default:
-    //             return '';
-    //     }
-    // };
 
     const handleClose = () => {
         setOpen(false);
@@ -93,7 +71,9 @@ export default function JobOfferApplyModal(props) {
                     <StyledGridItem>
                         <FormControlLabel control={<Checkbox
                             color="danger"
-                            onChange={handleChange}
+                            onChange={(e) => {
+                                setIsFirstBoxChecked(e.target.checked)
+                        }}
                             // onBlur={validateInput}
                             name = "r1"/>}
                          label="I hereby give consent for my personal data to be processed for the purpose of conducting recruitment for the position for which I am applying."/>
@@ -101,15 +81,12 @@ export default function JobOfferApplyModal(props) {
                     <p/>
                     <StyledGridItem>
                         <FormControlLabel control={<Checkbox color="danger"
-                             onChange={handleChange}
+                             onChange={(e) => {
+                                 setIsSecoundBoxChecked(e.target.checked)
+                             }}
                              // onBlur={validateInput}
                              name = "r2"/>}
                          label="I also consent to processing of my personal data for the purposes of any future recruitment processes."/>
-                        {state.r1.errors && <StyledText
-                            tag="span"
-                            color="red"
-                            text={state.r1.errors}
-                        />}
                     </StyledGridItem>
                     <StyledText tag={"h4"} color={"green"} text={appliedText} />
                 </DialogContent>
@@ -117,9 +94,15 @@ export default function JobOfferApplyModal(props) {
                     <RedButtonStyled onClick={handleClose}>
                         Discard
                     </RedButtonStyled>
-                    <RedButtonStyled onClick={handleSave}>
-                        Apply
-                    </RedButtonStyled>
+                    {isFirstBoxChecked && isSecoundBoxChecked ? (
+                        <RedButtonStyled disabled={false} onClick={handleSave}>
+                            Apply
+                        </RedButtonStyled>) : (
+                        <RedButtonStyled disabled={true}>
+                            Apply
+                        </RedButtonStyled>
+                    )
+                    }
                 </DialogActions>
 
             </Dialog>
