da8911725ae5065ab81c8f8fab015479cd8bc552
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-05-11 23:39:57 +0200

    Feature: Added github icon

---
+++ jobfever/src/components/atoms/StyledGithubIcon.js
@@ -0,0 +1,8 @@
+import {GitHub as MUIGitHubIcon} from '@mui/icons-material';
+import {styled} from "@mui/material";
+
+export const StyledGithubIcon = styled(MUIGitHubIcon)(() => ({
+    fontSize: '1.5em',
+    color: '#666',
+    marginRight: '1rem'
+}))
