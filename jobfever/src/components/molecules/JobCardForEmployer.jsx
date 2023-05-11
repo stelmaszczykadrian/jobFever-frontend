338534046a741fae01e0d04733579a2f136e7eb3
reizergrzegorz <reizergrzegorz@gmail.com>
2023-05-11 19:34:10 +0200

    Refactor: Add token checking to check if user is able to edit and delete jobs

--- jobfever/src/components/molecules/JobCardForEmployer.jsx
+++ jobfever/src/components/molecules/JobCardForEmployer.jsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React, {useMemo} from "react";
 import {JobTitleOnJobsCard, StyledPaperJobsCard} from "./JobsCard.styles";
 import IconButton from "@mui/material/IconButton";
 import {StyledEditIcon} from "../atoms/StyledEditIcon";
 import {StyledDeleteIcon} from "../atoms/StyledDeleteIcon";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import Cookies from "js-cookie";
 
 
-export function JobCardForEmployer({ job, handleJobClick, handleJobEdit, handleDeleteOffer, handleCandidatesClick }) {
+export function JobCardForEmployer({ employerId, job, handleJobClick, handleJobEdit, handleDeleteOffer, handleCandidatesClick }) {
 
     const handleCardClick = () => {
         handleJobClick(job.jobId);
     };
+
+    const isEmployer = useMemo(() => employerId === JSON.parse(Cookies.get("jwt")).employer_id.toString(),[employerId])
 
     return (
         <StyledPaperJobsCard>
             <JobTitleOnJobsCard  onClick={handleCardClick} variant="h1" component="h1">
                 {job.title}
             </JobTitleOnJobsCard>
-            <div>
+            {isEmployer && <div>
                 <IconButton onClick={() => handleJobEdit(job.jobId)}>
                     <StyledEditIcon/>
                 </IconButton>
@@ -27,7 +30,7 @@ export function JobCardForEmployer({ job, handleJobClick, handleJobEdit, handleD
                 <IconButton onClick={() => handleCandidatesClick(job.jobId, job.title)}>
                     <PeopleAltIcon style={{fill: "rgb(183, 4, 11)", fontSize: '1.4em'}}/>
                 </IconButton>
-            </div>
+            </div>}
         </StyledPaperJobsCard>
     );
 }
