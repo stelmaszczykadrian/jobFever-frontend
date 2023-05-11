0701fefaa94fd9875e0641296e01f809d8b66e03
stelmaszczykadrian <Tooyota12345@>
2023-05-11 20:16:37 +0200

    JOBF-139 Fix: Correct display of text colors in ApplyModal

--- jobfever/src/components/molecules/JobOfferApplyModal.jsx
+++ jobfever/src/components/molecules/JobOfferApplyModal.jsx
@@ -4,7 +4,6 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import {StyledGridItem} from "../organisms/JobOfferFormContainer.styles";
 import {RedButtonStyled} from "../atoms/RedButton.styles";
 import {Checkbox} from "@mui/joy";
 import {FormControlLabel} from "@mui/material";
@@ -12,9 +11,9 @@ import StyledText from "../atoms/StyledText";
 import Cookies from "js-cookie";
 import {applyForJob, checkIfCandidateApplied} from "../../api/JobsApi";
 import {useState} from "react";
+import {StyledApplyModalGridItem, StyledMessage} from "./JobOfferApplyModal.styles";
 
 export default function JobOfferApplyModal(props) {
-    const title = "Are You sure that You wanna apply for this offer?";
     let jwt = Cookies.get("jwt")
     const [open, setOpen] = useState(false);
     const [fullWidth] = useState(true);
@@ -33,35 +32,30 @@ export default function JobOfferApplyModal(props) {
     function timeout(delay) {
         return new Promise( res => setTimeout(res, delay) );
     }
-    // const handleSave = async () => {
-    //     const message = await applyForJob(props.jobId, JSON.parse(jwt).candidate_id)
-    //     setAppliedText(message)
-    //     await timeout(2000);
-    //     setOpen(false);
-    //     setAppliedText("")
-    // };
-
 
     const handleSave = async () => {
         const candidateId = JSON.parse(jwt).candidate_id;
         const jobId = props.jobId;
-
         const hasApplied = await checkIfCandidateApplied(candidateId, jobId);
+        const message = await applyForJob(jobId, candidateId);
+
         if (hasApplied) {
-            setAppliedText("You have already applied for this job");
+            setAppliedText(
+                <StyledMessage>You have already applied for this job</StyledMessage>
+            );
             await timeout(2000);
             setOpen(false);
             setAppliedText("");
             return;
         }
 
-        const message = await applyForJob(jobId, candidateId);
-        setAppliedText(message);
+        setAppliedText(
+            <StyledMessage>{message}</StyledMessage>
+        );
         await timeout(2000);
         setOpen(false);
         setAppliedText("");
     };
-
 
     const RenderApplyButton = () => {
         if (jwt){
@@ -78,19 +72,19 @@ export default function JobOfferApplyModal(props) {
         <div>
             <RenderApplyButton />
             <Dialog
-                fullWidth={fullWidth}
-                maxWidth={maxWidth}
+                fullWidth
+                maxWidth={'sm'}
                 open={open}
                 onClose={handleClose}
                 aria-labelledby="responsive-dialog-title"
             >
                 <DialogTitle sx={{textAlign : "center"}} id="responsive-dialog-title">
-                    {title}
+                    Are You sure that You wanna apply for this offer?
                 </DialogTitle>
                 <DialogContent>
                     <DialogContentText component="legend" sx={{textAlign : "center"}}> If You're sure, please accept the RODO to continue application process.</DialogContentText>
                     <p/>
-                    <StyledGridItem>
+                    <StyledApplyModalGridItem>
                         <FormControlLabel control={<Checkbox
                             color="danger"
                             onChange={(e) => {
@@ -98,16 +92,16 @@ export default function JobOfferApplyModal(props) {
                         }}
                             name = "r1"/>}
                          label="I hereby give consent for my personal data to be processed for the purpose of conducting recruitment for the position for which I am applying."/>
-                    </StyledGridItem>
+                    </StyledApplyModalGridItem>
                     <p/>
-                    <StyledGridItem>
+                    <StyledApplyModalGridItem>
                         <FormControlLabel control={<Checkbox color="danger"
                              onChange={(e) => {
                                  setIsSecondBoxChecked(e.target.checked)
                              }}
                              name = "r2"/>}
                          label="I also consent to processing of my personal data for the purposes of any future recruitment processes."/>
-                    </StyledGridItem>
+                    </StyledApplyModalGridItem>
                     <StyledText tag={"h4"} color={"black"} text={appliedText} />
                 </DialogContent>
                 <DialogActions>
---
+++ jobfever/src/components/molecules/JobOfferApplyModal.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+export const StyledApplyModalGridItem = styled("div")`
+  text-align: center;
+  margin: 12px 0px;
+ 
+  justify-content: space-between;
+  border-radius: 4px;
+  color: black;
+`
+
+export const StyledMessage = styled('p')`
+  color: red;
+`
