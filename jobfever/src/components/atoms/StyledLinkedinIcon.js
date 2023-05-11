e415ca28b6e6c99b2d8df742c0b8ebb240eb1653
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-05-11 11:49:42 +0200

    Feature: Added reusable email linkedin phone icons

---
+++ jobfever/src/components/atoms/StyledLinkedinIcon.js
@@ -0,0 +1,9 @@
+import {LinkedIn as MUILinkedInIcon} from '@mui/icons-material';
+import React from "react";
+import {styled} from "@mui/material";
+
+export const StyledLinkedInIcon = styled(MUILinkedInIcon)(() => ({
+    fontSize: '1.5em',
+    color: '#666',
+    marginRight: '1rem'
+}))
---
+++ jobfever/src/components/atoms/StyledPhoneIcon.js
@@ -0,0 +1,9 @@
+import {ContactPhone as MUIContactPhoneIcon} from '@mui/icons-material';
+import React from "react";
+import {styled} from "@mui/material";
+
+export const StyledContactPhoneIcon = styled(MUIContactPhoneIcon)(() => ({
+    fontSize: '1.5em',
+    color: '#666',
+    marginRight: '1rem'
+}))
