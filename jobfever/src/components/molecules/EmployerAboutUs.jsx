20ce73284bfed6c1e6bc612b6c0cc2c099c70982
stelmaszczykadrian <Tooyota12345@>
2023-05-11 12:28:45 +0200

    JOBF-137 Fix: Correctly display previous values in EmployerAboutUsModal

--- jobfever/src/components/molecules/EmployerAboutUs.jsx
+++ jobfever/src/components/molecules/EmployerAboutUs.jsx
@@ -8,37 +8,42 @@ import {useEmployerById} from "../../api/EmployersApi";
 import Cookies from "js-cookie";
 
 export default function EmployerAboutUs(props) {
-    const {data, loading} = useEmployerById(props.id)
+    const {data, loading} = useEmployerById(props.id);
     const [aboutUs, setAboutUs] = useState("");
+    const [previousAboutUs, setPreviousAboutUs] = useState("");
 
     React.useEffect(() => {
         if (!loading) {
-            setAboutUs(data.aboutUs)
+            setPreviousAboutUs(data.aboutUs);
+            setAboutUs(data.aboutUs);
         }
     }, [data]);
 
-    const RenderEmployerAboutUsModal= (props) => {
+    const RenderEmployerAboutUsModal = (props) => {
         if (props.id === JSON.parse(Cookies.get("jwt")).employer_id.toString()) {
-
             return (
                 <StyledIconBox>
-                    <EmployerAboutUsModal id={props.id} text={'About Us..'} tag={<StyledEditIcon/>}/>
+                    <EmployerAboutUsModal
+                        id={props.id}
+                        text={"About Us.."}
+                        tag={<StyledEditIcon/>}
+                        previousAboutUs={previousAboutUs}
+                    />
                 </StyledIconBox>
-            )
+            );
         }
-    }
-
+    };
 
     if (!loading) {
         return (
             <StyledProfilePaper>
                 <StyledTopBox>
-                    <ProfileContainerTitle text={'About Us'}/>
+                    <ProfileContainerTitle text={"About Us"}/>
                 </StyledTopBox>
                 <StyledBottomBox>
                     {aboutUs}
                     <Box>
-                        <RenderEmployerAboutUsModal id={props.id} />
+                        <RenderEmployerAboutUsModal id={props.id}/>
                     </Box>
                 </StyledBottomBox>
             </StyledProfilePaper>
