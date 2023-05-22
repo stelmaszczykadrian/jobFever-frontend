c1dbd845c7952255599393e27a7f1676f0e18817
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-05-22 11:11:04 +0200

    JOBF-151 Feature : Styling 'You don't remember password' text

--- jobfever/src/components/atoms/StyledLink.js
+++ jobfever/src/components/atoms/StyledLink.js
@@ -3,3 +3,10 @@ import {styled} from "@mui/material";
 export const StyledLink = styled("a")(() => ({
     color: 'black'
 }))
+
+
+export const StyledDontRememberPasswordLink = styled('a')`
+  color: red;
+  display: flex;
+  justify-content: center;
+`;
