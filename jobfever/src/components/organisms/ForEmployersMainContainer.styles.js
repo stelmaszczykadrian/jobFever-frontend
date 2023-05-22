4703f3596a2194e8d2118f90cd0b4002b633918d
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-05-22 11:42:48 +0200

    JOBF-151 Feature: Box styling on 'forEmployers' site

--- jobfever/src/components/organisms/ForEmployersMainContainer.styles.js
+++ jobfever/src/components/organisms/ForEmployersMainContainer.styles.js
@@ -2,9 +2,10 @@ import styled from "styled-components";
 
 export const StyledForEmployersMainContainer = styled('div')`
   width: 50%;
-  height: 100%;
-  margin: 0 auto;
-  background: rgba(29, 25, 23, 0.5);
+  height: 50%;
+  margin: 150px auto;
+  background: rgba(29, 25, 23, 0.6);
+  border-radius: 30px;
   text-align: center;
   display: flex;
   justify-content: center;
