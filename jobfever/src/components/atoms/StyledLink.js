6e1975f2c32238f4270cd438892a4b537ec6d93f
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-05-22 11:18:36 +0200

    JOBF-151 Fix: Make 'You don't remember password' text clickable to open modal

--- jobfever/src/components/atoms/StyledLink.js
+++ jobfever/src/components/atoms/StyledLink.js
@@ -1,11 +1,12 @@
 import {styled} from "@mui/material";
+import {Link} from "@mui/joy";
 
 export const StyledLink = styled("a")(() => ({
     color: 'black'
 }))
 
 
-export const StyledDontRememberPasswordLink = styled('a')`
+export const StyledDontRememberPasswordLink = styled(Link)`
   color: red;
   display: flex;
   justify-content: center;
