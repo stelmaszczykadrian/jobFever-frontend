8c893cf76b03f6fad172fc12f3eb474b12002286
Piotr Petryka <ppwxp19@gmail.com>
2023-05-22 11:20:52 +0200

    JOBF-148 - Feature: email sending

--- jobfever/src/components/organisms/ContactPageContainer.jsx
+++ jobfever/src/components/organisms/ContactPageContainer.jsx
@@ -9,8 +9,18 @@ import {
     StyledContactPageTextarea,
     StyledContactPageMainContainer
 } from "./ContactPageContainer.styles";
+import {useState} from "react";
+import {sendContactUs} from "../../api/ContactUsApi";
 
 export default function ContactPageContainer() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [message, setMessage] = useState("");
+    const handleSubmit = event => {
+        event.preventDefault();
+        sendContactUs(name, email, phone, message)
+    };
     return (
         <StyledContactPageMainContainer>
             <StyledContactPageContainer>
@@ -18,11 +28,15 @@ export default function ContactPageContainer() {
                     <StyledContactPageLeftContainer/>
                     <StyledContactPageRightContainer>
                         <StyledContactPageHeading>Contact Us</StyledContactPageHeading>
-                        <StyledContactPageInputField type="text" placeholder="Your Name"></StyledContactPageInputField>
-                        <StyledContactPageInputField type="text" placeholder="Your Email"></StyledContactPageInputField>
-                        <StyledContactPageInputField type="text" placeholder="Phone"></StyledContactPageInputField>
-                        <StyledContactPageTextarea placeholder="Message"></StyledContactPageTextarea>
-                        <StyledContactPageButton>Send</StyledContactPageButton>
+                        <form onSubmit={handleSubmit}>
+                        <StyledContactPageInputField type="text" placeholder="Your Name"
+                                                     name={"name"} value={name} onChange={event => setName(event.target.value)}>
+                        </StyledContactPageInputField>
+                        <StyledContactPageInputField type="text" placeholder="Your Email" name={"email"} value={email} onChange={event => setEmail(event.target.value)}></StyledContactPageInputField>
+                        <StyledContactPageInputField type="text" placeholder="Phone" name={"phone"} value={phone} onChange={event => setPhone(event.target.value)}></StyledContactPageInputField>
+                        <StyledContactPageTextarea placeholder="Message" name={"message"} value={message} onChange={event => setMessage(event.target.value)}></StyledContactPageTextarea>
+                        <StyledContactPageButton type={"submit"}>Send</StyledContactPageButton>
+                        </form>
                     </StyledContactPageRightContainer>
                 </StyledContactBox>
             </StyledContactPageContainer>
