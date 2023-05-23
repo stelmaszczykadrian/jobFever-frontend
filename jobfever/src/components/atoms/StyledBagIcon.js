b22b43f6f2acea9e381b39ab0e5b342eacd87dd0
reizergrzegorz <reizergrzegorz@gmail.com>
2023-05-23 13:06:47 +0200

    Feature: Add files with icons to single offer page.

---
+++ jobfever/src/components/atoms/StyledBagIcon.js
@@ -0,0 +1,8 @@
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import React from "react";
+import {styled} from "@mui/material";
+
+export const StyledBagIcon = styled(BusinessCenterIcon)(() => ({
+    fontSize: '3em',
+    color: 'rgb(183, 4, 11)',
+}))
