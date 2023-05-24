92e2586c6fc5046d8d745f8e342720dc2851bea4
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-05-24 14:47:19 +0200

    JOBF-131 Fix: Change pickers day from default color to red

--- jobfever/src/components/organisms/CalendarForm.jsx
+++ jobfever/src/components/organisms/CalendarForm.jsx
@@ -2,10 +2,42 @@ import * as React from 'react';
 import {DatePicker} from '@mui/x-date-pickers/DatePicker';
 import dayjs from "dayjs";
 import EditableModalInput from "../atoms/EditableModalInput";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+
+
+const theme = createTheme({
+    components: {
+        MuiPickersDay: {
+            styleOverrides: {
+                root: {
+                    '&.Mui-selected': {
+                        color: '#fff',
+                        backgroundColor: '#c0030b !important',
+                    },
+                    '&.MuiDay-today': {
+                        color: '#c0030b',
+                        backgroundColor: 'transparent !important',
+                    },
+                    '&:not(.Mui-selected)': {
+                        backgroundColor: 'transparent',
+                    },
+                    '&:hover': {
+                        backgroundColor: '#c0030b',
+                        color: '#fff',
+                    },
+                    '&:hover.Mui-selected': {
+                        backgroundColor: '#c0030b',
+                    },
+                },
+            },
+        },
+    },
+});
 
 export default function CalendarForm({name, date, setDate}) {
     let required = true;
     return (
+        <ThemeProvider theme={theme}>
         <DatePicker
             TextFieldComponent={EditableModalInput}
             required={required}
@@ -18,5 +50,6 @@ export default function CalendarForm({name, date, setDate}) {
                 setDate(e.format('YYYY-MM-DD'))
             }}
         />
+        </ThemeProvider>
     );
 }
