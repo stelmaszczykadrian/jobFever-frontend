af125a748e666073ffd66ff5517a46613ef073b5
reizergrzegorz <reizergrzegorz@gmail.com>
2023-05-25 20:27:01 +0200

    JOBF-144 Feature: Make employer box on single offer page clickable.

--- jobfever/src/components/organisms/SingleOfferRightContainer.jsx
+++ jobfever/src/components/organisms/SingleOfferRightContainer.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import {StyledSingleOfferRightContainer} from "./SingleOfferRightContainer.styles";
 import SingleOfferDetailsContainer from "../molecules/SingleOfferDetailsContainer";
 import SingleOfferEmployerNameField from "../molecules/SingleOfferEmployerNameField";
+import {useNavigate} from "react-router-dom";
 
 function SingleOfferRightContainer(props) {
 
+    const navigate = useNavigate();
+    const navigateToEmployerPage = (employerId) => {
+        navigate(`/employer/${employerId}`);
+    };
+    const handleEmployerClick = (employerId) => {
+        navigateToEmployerPage(employerId);
+    };
+
     return (
         <StyledSingleOfferRightContainer>
-            <SingleOfferEmployerNameField {...props}/>
+            <SingleOfferEmployerNameField {...props} onClick={() => handleEmployerClick(props.offerDetails.employer_id)}/>
             <SingleOfferDetailsContainer {...props}/>
         </StyledSingleOfferRightContainer>
     );
